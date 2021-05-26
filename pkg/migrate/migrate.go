05771d01b7f354ba1d02addc827a70917203147f
Mark St John <markstjohn@google.com>
2021-05-26 23:33:00 +0000

    Add Migrator interface. This interface is implemented by all resource that requires migration (i.e. Networks, Cluster, NodePools).
    Change-Id: I2abfaae939644695c27f7369cad72f455ec110b3

---
+++ pkg/migrate/migrate.go
@@ -0,0 +1,59 @@
+/*
+Copyright © 2021 Google
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package migrate
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type Migrator interface {
+	Migrate(ctx context.Context) error
+}
+
+// Run rate-limits the execution of Migrators based on the incoming semaphore.
+// An error which occurs during the Migrator.Migrate call will cease progressing
+// on that branch of the execution tree.
+func Run(ctx context.Context, sem chan struct{}, migrators ...Migrator) error {
+	var (
+		err error
+		wg = sync.WaitGroup{}
+	)
+
+Loop:
+	for _, m := range migrators {
+		select {
+		case <-ctx.Done():
+			log.Errorf("Context closed. Waiting for pending migrators to finish.")
+			err = fmt.Errorf("context error: %w", ctx.Err())
+			break Loop
+		case sem <- struct{}{}:
+		}
+		wg.Add(1)
+		go func(m Migrator) {
+			if err := m.Migrate(ctx); err != nil {
+				log.Errorf("Error during migration for %T: %v", m, err)
+			}
+			<-sem
+			wg.Done()
+		}(m)
+	}
+	wg.Wait()
+	return err
+}
---
+++ pkg/migrate/migrate_test.go
@@ -0,0 +1,97 @@
+/*
+Copyright © 2021 Google
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package migrate
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"strings"
+	"testing"
+
+	log "github.com/sirupsen/logrus"
+	"legacymigration/test"
+)
+
+func TestMigrate(t *testing.T) {
+	cancelled, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	select {
+	case <- cancelled.Done():
+	}
+
+	cases := []struct {
+		desc      string
+		migrators []Migrator
+		ctx       context.Context
+		wantErr   string
+		wantLog  string
+	}{
+		{
+			desc: "Empty migrators",
+			migrators: []Migrator{
+				&test.FakeMigrator{},
+			},
+			ctx:     context.Background(),
+			wantErr: "",
+		},
+		{
+			desc: "Successful run",
+			migrators: []Migrator{
+				&test.FakeMigrator{},
+			},
+			ctx:     context.Background(),
+			wantErr: "",
+		},
+		{
+			desc: "Errored migrator",
+			migrators: []Migrator{
+				&test.FakeMigrator{
+					Error: errors.New("expected error"),
+				},
+			},
+			ctx:     context.Background(),
+			wantLog: "expected error",
+		},
+		{
+			desc: "Closed context",
+			migrators: []Migrator{
+				&test.FakeMigrator{},
+				&test.FakeMigrator{},
+			},
+			ctx:     cancelled,
+			wantErr: "context error: context canceled",
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.desc, func(t *testing.T) {
+			buf := &bytes.Buffer{}
+			log.StandardLogger().SetOutput(buf)
+			sem := make(chan struct{}, 1)
+
+			got := Run(tc.ctx, sem, tc.migrators...)
+
+			if diff := test.ErrorDiff(tc.wantErr, got); diff != "" {
+				t.Fatalf("migrate.Run diff (-want +got):\n%s", diff)
+			}
+
+			if diff := !strings.Contains(buf.String(), tc.wantLog); tc.wantLog != "" && diff {
+				t.Errorf("migrate.Run missing log output:\n\twanted entry: %s\n\tgot entries: %s", tc.wantLog, buf.String())
+			}
+		})
+	}
+}
